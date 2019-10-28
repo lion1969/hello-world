b3786c11f0fa782eab22ff77b3db0196b807d27f
leonid <lion69@mail.ru>
2019-10-28 14:48:56 +0300

    using requests lib

--- http_reqests_sending.py
+++ http_reqests_sending.py
@@ -8,15 +8,16 @@ import requests
 #r = requests.get("https://192.168.2.222/relay.cgi?r2", params=payload)
 
 # Current status of relay
+print("Status of Relay#1:")
 r = requests.get("http://pitanie:turnonoff@192.168.2.222/relay.cgi?r1")
-
+print(r.text)
 # Turn OFF relay #N
 #r = requests.get("http://pitanie:turnonoff@192.168.2.222/relay.cgi?r1=0")
 
 # Turn ON relay #N
 #r = requests.get("http://pitanie:turnonoff@192.168.2.222/relay.cgi?r1=1")
 
-print(r.url)
-print("HTTP Status Code:", r.status_code)
-print("Content:", r.content)
-print(r.text)
+#print(r.url)
+#print("HTTP Status Code:", r.status_code)
+#print("Content:", r.content)
+#print(r.text)
