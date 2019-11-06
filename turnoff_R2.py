9d454fee6b64f83a551c342bb0391eda924af5d2
leonid <lion69@mail.ru>
2019-11-06 13:54:37 +0300

    using requests lib

---
+++ turnoff_R2.py
@@ -0,0 +1,7 @@
+import requests
+
+print("Turning Off Relay#1:")
+r = requests.get("http://pitanie:turnonoff@192.168.2.222/relay.cgi?r1=0")
+print("Status of Relay#1:")
+r = requests.get("http://pitanie:turnonoff@192.168.2.222/relay.cgi?r1")
+print(r.text)
